92940c52951e227d6089280f81fbdf123d496a5f
Raunak <rounand44@gmail.com>
2026-05-22 23:51:50 +0530

    fixed timeline

--- src/main/java/com/example/JobApplication/JobApplication.java
+++ src/main/java/com/example/JobApplication/JobApplication.java
@@ -3,11 +3,14 @@ package com.example.JobApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class JobApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        SpringApplication.run(JobApplication.class, args);
 	}
 
 }
